633461dfba552b95fb5ee3b612b809efcdd5dee0
Agustin de la Rosa <agustin.delarosa@mercadolibre.com>
2023-12-28 12:57:01 -0300

    First commit

---
+++ ej0.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func nombre() {
+	nombre, direccion := "Agustin de la Rosa -", " Av corrientes 939"
+	fmt.Println(nombre + direccion)
+}
+
+func clima(temperatura float32, presion float32, humedad int) {
+	fmt.Println("Temperatura = ", temperatura)
+	fmt.Println("Presion = ", presion)
+	fmt.Println("humedad = ", humedad)
+}
+
+func ej0() {
+	fmt.Println("Ejercicio 0:")
+	println()
+	nombre()
+	separadorCorto()
+	clima(27.5, 104.7, 64)
+	separador()
+}
---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func separador() {
+	fmt.Println("--------------------")
+}
+
+func separadorCorto() {
+	fmt.Println("------------")
+}
+
+func main() {
+	ej0()
+}
